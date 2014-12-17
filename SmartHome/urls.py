89c3fa3c8845b8d016a020470d12dd049544e11f
Blake Visin <Blake@oybro.com>
2014-12-17 15:25:51 -0800

    Upddated the icons and added a manifest for chrome to install a home screen icon

--- SmartHome/urls.py
+++ SmartHome/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import patterns
 from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf import settings
 
 from SmartHome.garage import urls as garage_urls
+from django.views.generic.simple import direct_to_template
 
 admin.autodiscover()
 
@@ -11,14 +13,17 @@ admin.autodiscover()
 # from django.contrib import admin
 # admin.autodiscover()
 
+context = {'PROJECT_NAME': settings.PROJECT_NAME, 'PROJECT_TITLE': settings.PROJECT_TITLE}
+
 urlpatterns = patterns('',
 
-    url(r'^$', 'SmartHome.home.views.home', name='home'),
-    (r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
-    (r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+		url(r'^$', 'SmartHome.home.views.home', name='home'),
+		(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+		(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 
-    url(r'^garage/', include(garage_urls)),
+		url(r'^garage/', include(garage_urls)),
+		(r'^manifest.json$', direct_to_template, {'extra_context': context, 'template': 'manifest.html', 'mimetype':'application/json'}),
 
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+		url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+		url(r'^admin/', include(admin.site.urls)),
 )
